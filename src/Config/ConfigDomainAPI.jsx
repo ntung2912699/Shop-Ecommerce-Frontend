ffa8b7b0e405a0fa9133236019753c9f12084e62
TungNV21 <104347621+TungNV21@users.noreply.github.com>
2022-10-11 00:03:29 +0700

    update config

--- src/Config/ConfigDomainAPI.jsx
+++ src/Config/ConfigDomainAPI.jsx
@@ -1,3 +1,3 @@
-const domainApi = "https://ecommerce-shop-api-project.herokuapp.com"
+const domainApi = "https://auto-smart-backend.herokuapp.com"
 // const domainApi = "http://127.0.0.1:8000"
 export default domainApi
