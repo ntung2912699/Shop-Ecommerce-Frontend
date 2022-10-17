6b1eacd2c61192cd03c332542fb8636cae8124d1
TungNV21 <104347621+TungNV21@users.noreply.github.com>
2022-10-17 11:21:53 +0700

    last update

--- src/Config/ConfigDomainAPI.jsx
+++ src/Config/ConfigDomainAPI.jsx
@@ -1,3 +1,3 @@
-const domainApi = "https://auto-smart-backend.herokuapp.com"
-// const domainApi = "http://127.0.0.1:8000"
+// const domainApi = "https://auto-smart-backend.herokuapp.com"
+const domainApi = "http://127.0.0.1:8000"
 export default domainApi
